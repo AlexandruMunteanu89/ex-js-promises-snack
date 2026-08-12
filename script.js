dd1acfcbd0537ba28255945ffeb4b184c02c66ba
AlexandruMunteanu89 <sasamunteanu18@gmail.com>
2026-08-12 19:13:35 +0200

    Fatto bonus snack 1

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Ottieni il titolo di un post con una Promise.
 
 // Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
+// 🎯 Bonus: Ottieni l'intero post con l'autore
+// Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
 const getPostTitle = id => {
     const promessa = new Promise((resolve, reject) => {
         fetch(`https://dummyjson.com/posts/${id}`)
@@ -10,8 +12,30 @@ const getPostTitle = id => {
     });
     return promessa;
 }
-getPostTitle(3)
+getPostTitle(2)
 .then(obj => console.log(obj))
 .catch(error => console.error(error));
-// 🎯 Bonus: Ottieni l'intero post con l'autore
-// Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
+
+function getPost(id){
+    return new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+        .then(response => response.json())
+        .then(post => {
+            fetch(`https://dummyjson.com/users/${post.userId}`)
+                .then(response => response.json())
+                .then(user => {
+                    const result = {
+                        ...post,
+                        user
+                    }
+                    resolve(result);
+                })
+                .catch(reject)
+        })
+        .catch(reject)
+    });
+}
+
+getPost(2)
+.then(post => console.log(post))
+.catch(error => console.error(error));
